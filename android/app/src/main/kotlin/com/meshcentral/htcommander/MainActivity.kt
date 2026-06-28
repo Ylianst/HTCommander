b19bbfae6be438d7826aa3fb26b401f2b825a7dd
ylianst <ylianst@gmail.com>
2026-06-28 11:56:59 -0700

    Android package rename

---
+++ android/app/src/main/kotlin/com/meshcentral/htcommander/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.meshcentral.htcommander
+
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+
+class MainActivity : FlutterActivity() {
+    private var bluetoothClassicPlugin: BluetoothClassicPlugin? = null
+
+    override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
+        super.configureFlutterEngine(flutterEngine)
+        val plugin = BluetoothClassicPlugin(
+            applicationContext,
+            flutterEngine.dartExecutor.binaryMessenger,
+        )
+        plugin.activity = this
+        bluetoothClassicPlugin = plugin
+    }
+}
